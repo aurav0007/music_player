e8d4e4ebfc67498b757641a3c3a58566eda6c29a
aurav0007 <kesarwanigaurav15@gmail.com>
2023-04-13 20:11:28 +0530

    DAY4

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,45 @@
 import React from 'react'
 
 import Artist from '../img/Artist.jpg'
-// import check from '../img/verfied.png'
-import bk from '../img/backjpg.jpg'
+import verified from '../img/verified.png'
+
+import {FaEllipsisH, FaHeadphones, FaCheck} from 'react-icons/fa'
 function Banner() {
   return (
     <div className='banner'>
         <img src={Artist} alt ='' className='bannerImg'></img>
-      
+         <div className='content'>
+          <div className='breadCrump'>
+            <p>
+              Home <span>/Popular Artist</span>
+            </p>
+            <i>
+              <FaEllipsisH />
+            </i>
+          </div>
+          <div className='artist'>
+            <div className='left'>
+             <div className='name'>
+              <h2>AR</h2>
+              <img src={verified} alt =''></img>
+             </div>
+             <p>
+              <i><FaHeadphones /></i>
+              11,184,126 <span> Monthly listeners</span>
+             </p>
+            </div>
+            <div className='right'>
+              <a href='#'>Play</a>
+              <a href='#'>
+              <i><FaCheck /></i>
+                Following
+              </a> 
+            </div>
+          </div>
+         </div>
+         <div className='bottomLayer'></div>
     </div>
-  )
+  );
 }
 
 export { Banner }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import { Banner } from './Banner';
 
 import '../styles/MainContainer.css';
+import { FaUser } from 'react-icons/fa';
 
 function MainContainer() {
+
+  useEffect(() => {
+    const allLi = document.querySelector('.menuList ul').querySelectorAll("li");
+     
+    function changeMenuActive() {
+      allLi.forEach(n => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+    // console.log(allLi);
+    allLi.forEach( n => n.addEventListener('click',changeMenuActive));
+
+  }, [])
+
   return (
     <div className='mainContainer'>
       <Banner />
+      <div className='menuList'>
+        <ul>
+          <li><a href='#'>Popular</a></li>
+          <li><a href='#'>Albums</a></li>
+          <li><a href='#'>Songs</a></li>
+          <li><a href='#'>Fans</a></li>
+          <li><a href='#'>About</a></li>
+        </ul>
+        <p>
+          <i><FaUser /></i>
+          12.3M <span>Followers</span>
+        </p>
+      </div>
     </div>
   )
 }
